bcbc0208488453597ee2f1d086e31a5921a14da7
coboyle444 <courtlyn.oboyle@edu.sait.ca>
2025-04-10 21:24:53 -0600

    trying to get weather icon

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&APPID=b473fd5304
 .then(response => response.json ())
 .then(data => {
     console.log(data);
-    comsole.log(data.main.temp);
+    console.log(data.main.temp);
     weather.textContent = "Current Weather in Cancun : " +data.main.temp+" \u00B0C";
 });
+
+const iconCode = data.weather[0].icon;
+const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+document.getElementById('weathericon').src = iconUrl;
+document.getElementById('weathericon').alt = data.weather[0].description;
+console.log("Weather icon url", iconUrl);
+
+
 
 /* does not work */
 
